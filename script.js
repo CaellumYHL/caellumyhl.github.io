96c896b58381fb5236d31f0656dad4ccac783d3b
Caellum <80055159+CaellumYHL@users.noreply.github.com>
2025-09-21 14:10:04 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('btnA').addEventListener('click', function() {
+  document.getElementById('content').textContent = "Here is the text for A.";
+});
+document.getElementById('btnB').addEventListener('click', function() {
+  document.getElementById('content').textContent = "Here is the text for B.";
+});
